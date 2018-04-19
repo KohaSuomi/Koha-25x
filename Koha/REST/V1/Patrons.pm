7a2387fc2f7e77384064f63f5455ed876e1e4060
Johanna Raisa <johanna.raisa@gmail.com>
2018-04-19 10:58:21 +0300

    KD-2473: GDPR logging improvements

--- Koha/REST/V1/Patrons.pm
+++ Koha/REST/V1/Patrons.pm
@@ -56,6 +56,10 @@ sub list {
         my $patrons_rs = Koha::Patrons->search($query);
         my $patrons    = $c->objects->search($patrons_rs);
 
+        foreach my $patron (@{$patrons}) {
+            C4::Log::logaction("MEMBERS", "VIEW", $patron->{patron_id}, "Patrons search") if C4::Context->preference("BorrowersViewLog");
+        }
+
         return $c->render(
             status  => 200,
             openapi => $patrons
@@ -80,6 +84,8 @@ sub get {
 
         return $c->render_resource_not_found("Patron")
             unless $patron;
+
+        C4::Log::logaction("MEMBERS", "VIEW", $patron->{borrowernumber}, "Patron selection") if C4::Context->preference("BorrowersViewLog");
 
         return $c->render(
             status  => 200,
