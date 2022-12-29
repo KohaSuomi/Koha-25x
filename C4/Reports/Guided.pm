971bdeb9339ab69d002dd681bcfcbc205d7e6001
lmstrand <lmstrand@gmail.com>
2022-12-29 15:15:16 +0200

    KD-5239 use altdbh for reports

--- C4/Reports/Guided.pm
+++ C4/Reports/Guided.pm
@@ -39,6 +39,8 @@ use Koha::Reports;
 use Koha::SharedContent;
 use Koha::TemplateUtils qw( process_tt );
 
+use C4::KohaSuomi::Tweaks;
+
 our ( @ISA, @EXPORT_OK );
 
 BEGIN {
@@ -640,6 +642,7 @@ sub execute_query {
     }
     $sql .= " LIMIT ?, ?";
 
+    # This will need to be done on master, since it is an update
     my $dbh = C4::Context->dbh;
 
     $dbh->do( 'UPDATE saved_sql SET last_run = NOW() WHERE id = ?', undef, $report_id ) if $report_id;
@@ -650,6 +653,9 @@ sub execute_query {
     Koha::Logger->get( { prefix => 0, interface => 'reports', category => 'execute.time.start' } )
         ->info("Report starting: $report_id")
         if $report_id;
+
+    # Select can be run on althost if configured, initialize a new database handle
+    $dbh=C4::KohaSuomi::Tweaks->dbh();
 
     my $sth = $dbh->prepare($sql);
     eval { $sth->execute( @$sql_params, $offset, $limit ); };
