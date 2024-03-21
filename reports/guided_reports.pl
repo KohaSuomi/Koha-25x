2fdd80dcdaf67fd81565a9c1bbc94607762067d2
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2024-03-21 15:00:03 +0200

    KD-4635 Add fines to overdue notice triggers
    Add fine-fields to overdue notice triggers for charging the patrons
    with overdue items. Fines are added with Koha-Suomi specific
    version of overdue_notices.pl that lives in koha-suomi-utility.

--- reports/guided_reports.pl
+++ reports/guided_reports.pl
@@ -643,6 +643,14 @@ if ( !$op ) {
             while ( my $row = $sth->fetchrow_arrayref() ) {
                 $content .= $scrubber->scrub( join( "\t", map { $_ // '' } @$row ) ) . "\n";
             }
+        } elsif ($format eq 'finna') {
+            $type = 'application/json';
+            my @biblios;
+            while (my $row = $sth->fetchrow_hashref()) {
+                push(@biblios, $row->{'biblionumber'}) if ($row->{'biblionumber'});
+            }
+            $reportfilename = $reportfilename.".json";
+            $content = finnajson(\@biblios, $reportname);
         } else {
             if ( $format eq 'csv' ) {
                 my $delimiter = C4::Context->csv_delimiter;
@@ -1099,6 +1107,32 @@ if ( $op eq 'list' || $op eq 'convert' ) {
         filters                 => $filter,
         has_obsolete_reports    => $has_obsolete_reports,
     );
+}
+
+#################################################
+# pick shelves data from array-reference and make json string (finna format)
+sub  finnajson {
+  my @args=@_;
+  my $biblios_ref=$args[0];
+  my $shelfname=$args[1];
+  my $finnaprefix = C4::Context->preference('finnaprefix');
+  my $json_string="{\n \"lists\":[{\n\"description\": null,\n\"public\": 0,\n\"records\":[\n";
+  my $i=0;
+  my $count=scalar @$biblios_ref;
+     while($i < $count)
+     {
+       if($i >0)
+       {
+         $json_string.=",";
+       }
+
+       $json_string.="{\n\"id\":\"".$finnaprefix.$biblios_ref->[$i]."\",\n";
+       $json_string.="\"notes\":null,\n\"order\":$i,\n\"source\":\"Solr\",\n\"tags\":[]\n}";
+       $i++;
+     }
+     $json_string.="\n],\n\"title\":\"".$shelfname."\"\n\}\n],\"searches\":[]\n}";
+     $json_string = Encode::encode('UTF-8', $json_string);
+     return($json_string);
 }
 
 # pass $sth, get back an array of names for the column headers
