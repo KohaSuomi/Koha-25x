cb398455946828df8d2b51a4d86318956b999e76
Johanna Räisä <johanna.raisa@gmail.com>
2024-10-24 09:31:39 +0300

    Bug 23269: use REST API for patron_holds_table
    This patch uses the REST API to populate the holds table
    
    Test plan:
    1) Apply the patch
    2) Go to a record with holds
    3) Check that the holds table is populated correctly
    4) Check that hold tables functions work correctly
    5) Change HoldsSplitQueue and HoldsSplitQueueNumbering system preference, try all variations
    6) Repeat steps 3 and 4
    7) Search for records and place holds to multiple records
    8) Repeat steps 3 and 4
    9) Try with lesser permissions
    
    Sponsored-by: Koha-Suomi Oy

--- koha-tmpl/intranet-tmpl/prog/js/datatables.js
+++ koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -1413,6 +1413,9 @@ function update_search_description(
                     emptyTable: options.emptyTable
                         ? options.emptyTable
                         : __("No data available in table"),
+                    infoFiltered: options.infoFiltered
+                        ? options.infoFiltered
+                        : __("(filtered from _MAX_ total entries)"),
                 },
             },
             options
