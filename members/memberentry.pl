a391d43bc7f21f3e41d48f157f9c4f0f4361a158
lmstrand <lmstrand@gmail.com>
2025-01-16 11:43:03 +0200

    G1606 update lastseen on borrower modification

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -567,8 +567,8 @@ if ( ( !$nok ) and $nodouble and ( $op eq 'cud-insert' or $op eq 'cud-save' ) ) 
 
         try {
             $patron->set( \%newdata )->store( { guarantors => \@guarantors } ) if scalar( keys %newdata ) > 1;
-
-            # bug 4508 - avoid crash if we're not updating any columns in the borrowers table (editing patron attrs or msg prefs)
+            $patron->update_lastseen('modification');
+                # bug 4508 - avoid crash if we're not updating any columns in the borrowers table (editing patron attrs or msg prefs)
             $success = 1;
         } catch {
             $success = 0;
