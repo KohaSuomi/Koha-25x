602c56b7688b5914725c96cb2b88feef4592a0af
Tomas Cohen Arazi <tomascohen@theke.io>
2025-06-30 15:11:30 -0300

    Bug 40275: Use Koha::Patrons->find_by_identifier in the codebase
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -4539,9 +4539,7 @@ Missing POD for ProcessOfflineIssue.
 sub ProcessOfflineIssue {
     my $operation = shift;
 
-    my $patron = Koha::Patrons->find( { cardnumber => $operation->{cardnumber} } );
-    $patron ||= Koha::Patrons->find( { userid => $operation->{cardnumber} } );
-    
+    my $patron = Koha::Patrons->find_by_identifier( $operation->{cardnumber} );
     my $no_block_prevreserve_status = $operation->{no_block_prevreserve_status};
 
     if ($patron) {
@@ -4583,8 +4581,7 @@ Missing POD for ProcessOfflinePayment.
 sub ProcessOfflinePayment {
     my $operation = shift;
 
-    my $patron = Koha::Patrons->find( { cardnumber => $operation->{cardnumber} } );
-    $patron ||= Koha::Patrons->find( { userid => $operation->{cardnumber} } );
+    my $patron = Koha::Patrons->find_by_identifier( $operation->{cardnumber} );
 
     $patron->account->pay(
         {
