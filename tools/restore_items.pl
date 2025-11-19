9212f3fc3e90c4e456ec8f2916ce35c27d26d950
lmstrand <lmstrand@gmail.com>
2025-11-19 12:02:58 +0200

    Fix edit_catalogue permissions

--- tools/restore_items.pl
+++ tools/restore_items.pl
@@ -42,7 +42,7 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         template_name => 'tools/restore_items.tt',
         query         => $input,
         type          => "intranet",
-        flagsrequired => { tools => 'edit_catalogue' },
+        flagsrequired => { catalogue => 'edit_catalogue' },
     }
 );
 
