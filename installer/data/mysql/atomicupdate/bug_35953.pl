1235e5461d423fc23d2703c5e41b7b2a9c39aaaf
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2025-12-05 11:12:13 +0200

    Bug 35953: Rename delete_any_record as delete_bibliographic_records
    Rename new permission for deleting bibliographic records as
    delete_any_record. Also rename atomicupdate file and update it to
    follow current skeleton.pl file example.
    
    To test:
    1. Apply this patch.
    2. Run updatedatabase.pl.
    => Confirm this new permission was added to Koha.
    3. Follow test plan from previous patch.
    => Confirm everything works as described.
    
    Sponsored by: Koha-Suomi Oy

---
+++ installer/data/mysql/atomicupdate/bug_35953.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "35953",
+    description => "Add new permission delete_bibliographic_records",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (9, 'delete_bibliographic_records', 'Delete bibliographic records from catalogue')}
+        );
+
+        say $out "Added new permission delete_bibliographic_records";
+
+        say $out "Add new permission delete_bibliographic_records for patrons";
+    },
+};
