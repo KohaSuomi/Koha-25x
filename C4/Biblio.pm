c26dfd37d8da1a4b827653b7ba22d8f3f558c640
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2026-02-02 08:19:17 +0200

    Automatic framework code plugin calls
    Adds calls to a plugin hook called "automatic_frameworkcode"
    when saving a new biblio or when bringing in one via Z35.90
    
    The plugin hook is called with two parameters: "frameworkcode"
    (the current framework code), and "record" which is the MARC record

--- C4/Biblio.pm
+++ C4/Biblio.pm
@@ -217,6 +217,8 @@ sub AddBiblio {
         return;
     }
 
+    Koha::Plugins->call('automatic_frameworkcode', { 'frameworkcode' => \$frameworkcode, 'record' => $record });
+
     my $schema = Koha::Database->schema;
     my ( $biblionumber, $biblioitemnumber );
     try {
@@ -433,6 +435,8 @@ sub ModBiblio {
     my $dbh = C4::Context->dbh;
 
     $frameworkcode = "" if !$frameworkcode || $frameworkcode eq "Default";    # XXX
+
+    Koha::Plugins->call('automatic_frameworkcode', { 'frameworkcode' => \$frameworkcode, 'record' => $record });
 
     _strip_item_fields( $record, $frameworkcode );
 
