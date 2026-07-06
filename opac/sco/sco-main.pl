3bea5b9da942200c38ff8c07c682cc4889d0a42d
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2026-07-06 10:48:36 +0300

    KOHA-2405: Clear JWT cookie on SCO
    If JWT cookie is not properly cleared after check in session
    is closed, Finish button can become inactive.
    
    See community bug 42634.

--- opac/sco/sco-main.pl
+++ opac/sco/sco-main.pl
@@ -431,7 +431,7 @@ $cookie = $cookie_mgr->replace_in_list(
     $query->cookie(
         -name     => 'JWT',
         -value    => $jwt // '',
-        -expires  => '+12M',
+        -expires  => $jwt ? '+12M' : '-12M',
         -HttpOnly => 1,
         -secure   => ( C4::Context->https_enabled() ? 1 : 0 ),
         -sameSite => 'Lax'
